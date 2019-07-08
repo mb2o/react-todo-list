1cd8abb7afb1b3e74b5bbcd40b4c6ea56c86f461
mb2o <mboomaars@gmail.com>
2019-07-08 14:15:19 +0200

    App Component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="container">
-      <div className="row bg-warning">
+      <div className="row">
         <TodoInput />
         <TodoList />
       </div>
