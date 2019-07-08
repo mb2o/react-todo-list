bd4776a9ae95089cb334c6ace3b2a32297e0769a
mb2o <mboomaars@gmail.com>
2019-07-08 14:29:13 +0200

    Input Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import uuid from 'uuid';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="container">
-      <div className="row">
-        <TodoInput />
-        <TodoList />
+class App extends Component {
+  state = {
+    items: [
+      { id: 1, title: 'Vakantiespullen inpakken' },
+      { id: 2, title: 'Boodschappen doen' },
+      { id: 3, title: 'Hond uitlaten' }
+    ],
+    id: uuid(),
+    item: '',
+    editItem: false
+  };
+
+  handleChange = e => {
+    console.log('Handle change');
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log('Submitting Item');
+  };
+
+  handleClear = () => {
+    console.log('List cleared');
+  };
+
+  handleEdit = id => {
+    console.log(id);
+  };
+
+  handleDelete = id => {
+    console.log(id);
+  };
+
+  render() {
+    console.log(this.state);
+
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-4">
+            <h3 className="text-capitalize text-center">Todo Input</h3>
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              editItem={this.state.editItem}
+            />
+            <TodoList
+              items={this.state.items}
+              handleClear={this.handleClear}
+              handleDelete={this.handleDelete}
+              handleEdit={this.handleEdit}
+            />
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,9 +2,29 @@ import React, { Component } from 'react';
 
 export default class TodoInput extends Component {
   render() {
+    const { item, handleChange, handleSubmit, editItem } = this.props;
+
     return (
-      <div>
-        <h2>Todo Input</h2>
+      <div className="card card-body my-3">
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="input-group-text bg-primary text-white">
+                <i className="fas fa-book" />
+              </div>
+            </div>
+            <input
+              type="text"
+              className="form-control text-capitalize"
+              placeholder="Add todo item"
+              value={item}
+              onChange={handleChange}
+            />
+          </div>
+          <button className="btn btn-block btn-primary mt-3 text-uppercase">
+            Add Item
+          </button>
+        </form>
       </div>
     );
   }
